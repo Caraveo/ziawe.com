b87b6e43aef5c32c472ce044c7f117ec0caabe1e
Jon <87449181+Caraveo@users.noreply.github.com>
2023-10-29 18:39:15 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,8 @@ export default function Home() {
           allow='autoplay; encrypted-media'
           allowFullScreen
           title='video'
+          width="{80%}" 
+          height="{60%}" 
         />
 
       </div>
